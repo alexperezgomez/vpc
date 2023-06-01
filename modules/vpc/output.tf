d3261e34ea573032bb27297e50e1e2f8aa18a773
alexperezgomez <130483196+alexperezgomez@users.noreply.github.com>
2023-06-01 09:03:58 -0500

    Add files via upload

---
+++ modules/vpc/output.tf
@@ -0,0 +1,4 @@
+output "subnet_private" {
+  value       = aws_subnet.PrivSubnet1.id
+  description = "identificador subnet privada 1"
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,61 @@
+/*variable "cidr_vpc" {
+  description = "cidr de la vpc"
+  type        = string
+}*/
+
+variable "range_vpc" {
+  description = "cidr de la vpc"
+  type        = string
+}
+
+
+variable "cidr_subnet_public1" {
+  description = "cidr para la subnet publica 1"
+  type        = string
+}
+
+
+variable "cidr_subnet_public2" {
+  description = "cidr para la subnet publica 2"
+  type        = string
+}
+
+
+variable "cidr_subnet_private1" {
+  description = "cidr para la subnet privada  1"
+  type        = string
+}
+
+
+variable "cidr_subnet_private2" {
+  description = "cidr para la subnet privada  2"
+  type        = string
+}
+
+variable "tags" {
+    type = map(string)
+}
+
+
+variable "availability_zone_public1" {
+  description = "cidr para la subnet publica 1"
+  type        = string
+}
+
+
+variable "availability_zone_public2" {
+  description = "cidr para la subnet publica 1"
+  type        = string
+}
+
+
+variable "availability_zone_private1" {
+  description = "cidr para la subnet publica 1"
+  type        = string
+}
+
+
+variable "availability_zone_private2" {
+  description = "cidr para la subnet publica 1"
+  type        = string
+}
